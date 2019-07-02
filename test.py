d2d216fe562ce7caa7630f57b4d6b7aed4ee68ac
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-07-02 18:40:00 -0400

    deleted tests

--- test.py
+++
@@ -1,4 +0,0 @@
-from constants import *
-EPSILON = 100
-print("hello world from test 1")
-x = 3
--- test2.py
+++
@@ -1,5 +0,0 @@
-import test
-import constants
-
-print(test.x, ' is the x from test 1')
-print(constants.EPSILON, ' is epsilon')
